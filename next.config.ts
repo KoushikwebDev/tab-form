b9cd5ad3411a4a63513ac31392cff683e73d6a92
koushikwebdev <kou.saha.ami@gmail.com>
2025-01-28 21:06:01 +0530

    chnaged title & desc, modified the next config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require("next-pwa")({
+  dest: "public", // PWA files destination
+  disable: false,
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = withPWA({
+  eslint: {
+    // Disable ESLint during the build process
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  // removeConsole: true,
+});
 
 export default nextConfig;
